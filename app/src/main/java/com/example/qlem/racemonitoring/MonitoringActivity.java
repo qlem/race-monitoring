f1344c7bfa3d89ace7212cc0f20cabf43fc1c0b8
qlem <clement.dommerc@gmail.com>
2018-11-22 21:54:09 +0000

    remove unused import

--- app/src/main/java/com/example/qlem/racemonitoring/MonitoringActivity.java
+++ app/src/main/java/com/example/qlem/racemonitoring/MonitoringActivity.java
@@ -5,7 +5,6 @@ import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.text.NumberFormat;
 import java.util.List;
