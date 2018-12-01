f1f5884f06172ff9200c20a8835e69862924b066
qlem <clement.dommerc@gmail.com>
2018-12-01 16:40:52 +0000

    add comments

--- app/src/main/java/com/example/qlem/racemonitoring/RaceReportActivity.java
+++ app/src/main/java/com/example/qlem/racemonitoring/RaceReportActivity.java
@@ -37,17 +37,21 @@ public class RaceReportActivity extends AppCompatActivity {
     private List<Speed> speeds;
 
     /**
-     * Some other important values.
+     * Some other important values about the locations list.
      */
+    private float distance;
     private float altMax;
     private float altMin;
+    private float altGain;
+    private float altLoss;
     private float speedMax;
     private float speedMin;
+    private float speedTotal;
 
     /**
      * Function that sets the view of the race duration.
      */
-    private void displayTimeRace() {
+    private void setDurationView() {
         TextView timeView = findViewById(R.id.time_value);
 
         if (locations.size() == 1) {
@@ -73,10 +77,63 @@ public class RaceReportActivity extends AppCompatActivity {
     }
 
     /**
+     * Function that is called for compute the speeds of the race.
+     * @param index index of the current location point of the list
+     */
+    private void computeSpeeds(int index) {
+        Location current = locations.get(index);
+
+        if (index < locations.size() - 1) {
+            float time = (locations.get(index + 1).getTime() - current.getTime()) / 1000;
+            float dist = current.distanceTo(locations.get(index + 1));
+            Speed speed = new Speed(dist / time);
+            if (index == 0) {
+                speedMin = speed.speed;
+                speedMax = speed.speed;
+            }
+            if (speed.speed > speedMax) {
+                speedMax = speed.speed;
+            } else if (speed.speed < speedMin) {
+                speedMin = speed.speed;
+            }
+            speedTotal += speed.speed;
+            speeds.add(speed);
+        }
+    }
+
+    /**
+     * Function that is called for compute the altitudes of the race.
+     * @param index index of the current location point of the list
+     */
+    private void computeAltitudes(int index) {
+        Location current = locations.get(index);
+
+        Altitude altitude = new Altitude((float) current.getAltitude());
+        if (index == 0) {
+            altMax = altitude.altitude;
+            altMin = altitude.altitude;
+        }
+        if (altitude.altitude > altMax) {
+            altMax = altitude.altitude;
+        } else if (altitude.altitude < altMin) {
+            altMin = altitude.altitude;
+        }
+        if (index < locations.size() - 1) {
+            double nextAltitude = locations.get(index + 1).getAltitude();
+            if (nextAltitude > altitude.altitude) {
+                altGain += (nextAltitude - altitude.altitude);
+            } else if (nextAltitude < altitude.altitude) {
+                altLoss += (nextAltitude - altitude.altitude);
+            }
+        }
+        altitudes.add(altitude);
+    }
+
+    /**
      * Function that sets the distance information view, the altitude information view and
      * the speed information view.
      */
-    private void displayRaceOverview() {
+    private void setOtherViews() {
 
         // distance views
         TextView distanceValueView = findViewById(R.id.distance_value);
@@ -92,65 +149,19 @@ public class RaceReportActivity extends AppCompatActivity {
         TextView altGainView = findViewById(R.id.elevation_gain_value);
         TextView altLossView = findViewById(R.id.elevation_loss_value);
 
-        // init vars
-        float distance = 0;
-        speedMax = 0;
-        speedMin = 0;
-        float speedTotal = 0;
-        altMax = 0;
-        altMin = 0;
-        float altGain = 0;
-        float altLoss = 0;
-
-        // loop
+        // loop through collections
         for (int i = 0; i < locations.size(); i++) {
-
-            // init current location
-            Location current = locations.get(i);
 
             // compute distance
             if (i < locations.size() - 1) {
-                distance += current.distanceTo(locations.get(i + 1));
+                distance += locations.get(i).distanceTo(locations.get(i + 1));
             }
 
-            // compute speed
-            if (i < locations.size() - 1) {
-                float time = (locations.get(i + 1).getTime() - current.getTime()) / 1000;
-                float dist = current.distanceTo(locations.get(i + 1));
-                Speed speed = new Speed(dist / time);
-                if (i == 0) {
-                    speedMin = speed.speed;
-                    speedMax = speed.speed;
-                }
-                if (speed.speed > speedMax) {
-                    speedMax = speed.speed;
-                } else if (speed.speed < speedMin) {
-                    speedMin = speed.speed;
-                }
-                speedTotal += speed.speed;
-                speeds.add(speed);
-            }
+            // compute speeds
+            computeSpeeds(i);
 
-            // compute altitude
-            Altitude altitude = new Altitude((float) current.getAltitude());
-            if (i == 0) {
-                altMax = altitude.altitude;
-                altMin = altitude.altitude;
-            }
-            if (altitude.altitude > altMax) {
-                altMax = altitude.altitude;
-            } else if (altitude.altitude < altMin) {
-                altMin = altitude.altitude;
-            }
-            if (i < locations.size() - 1) {
-                double nextAltitude = locations.get(i + 1).getAltitude();
-                if (nextAltitude > altitude.altitude) {
-                    altGain += (nextAltitude - altitude.altitude);
-                } else if (nextAltitude < altitude.altitude) {
-                    altLoss += (nextAltitude - altitude.altitude);
-                }
-            }
-            altitudes.add(altitude);
+            // compute altitudes
+            computeAltitudes(i);
         }
 
         NumberFormat nf = NumberFormat.getInstance();
@@ -159,6 +170,7 @@ public class RaceReportActivity extends AppCompatActivity {
         if (distance >= 1000) {
             distance = distance / 1000;
             nf.setMaximumFractionDigits(2);
+            nf.setMinimumFractionDigits(2);
             distanceValueView.setText(String.format("%skm", nf.format(distance)));
         } else {
             nf.setMaximumFractionDigits(0);
@@ -172,6 +184,7 @@ public class RaceReportActivity extends AppCompatActivity {
         }
         speeds.add(0, new Speed(avg));
         nf.setMaximumFractionDigits(1);
+        nf.setMinimumFractionDigits(1);
         speedMaxView.setText(nf.format(speedMax));
         speedMinView.setText(nf.format(speedMin));
         speedAvgView.setText(nf.format(avg));
@@ -201,9 +214,19 @@ public class RaceReportActivity extends AppCompatActivity {
         altitudes = new ArrayList<>();
         speeds = new ArrayList<>();
 
-        // display race overview
-        displayTimeRace();
-        displayRaceOverview();
+        // init vars
+        distance = 0;
+        speedMax = 0;
+        speedMin = 0;
+        speedTotal = 0;
+        altMax = 0;
+        altMin = 0;
+        altGain = 0;
+        altLoss = 0;
+
+        // set views
+        setDurationView();
+        setOtherViews();
 
         // init an object that contains altitudes / speeds lists and other important variables
         // and set the collection of the graph view.
