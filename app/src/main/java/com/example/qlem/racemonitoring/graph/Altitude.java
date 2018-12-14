96dc0128699db701c9f833b531e5d015e3cd395b
qlem <clement.dommerc@gmail.com>
2018-12-14 15:31:11 +0000

    add headers

--- app/src/main/java/com/example/qlem/racemonitoring/graph/Altitude.java
+++ app/src/main/java/com/example/qlem/racemonitoring/graph/Altitude.java
@@ -21,29 +21,58 @@ public class Altitude implements Parcelable {
         this.altitude = altitude;
     }
 
-    // TODO add headers
-
+    /**
+     * This constructor is used for retrieve the values that originally wrote into the Parcel.
+     * @param in the Parcel
+     */
     private Altitude(Parcel in) {
         altitude = in.readFloat();
     }
 
+    /**
+     * Interface that must be implemented and provided as a public CREATOR field that generates
+     * instances of the Parcelable class from a Parcel.
+     */
     public static final Creator<Altitude> CREATOR = new Creator<Altitude>() {
+
+        /**
+         * Create a new instance of the Parcelable class, instantiating it from the given Parcel
+         * whose data had previously been written by Parcelable.writeToParcel().
+         * @param in the Parcel to read the object's data from
+         * @return a new instance of the Parcelable class
+         */
         @Override
         public Altitude createFromParcel(Parcel in) {
             return new Altitude(in);
         }
 
+        /**
+         * Create a new array of the Parcelable class.
+         * @param size size of the array
+         * @return an array of the Parcelable class, with every entry initialized to null
+         */
         @Override
         public Altitude[] newArray(int size) {
             return new Altitude[size];
         }
     };
 
+    /**
+     * Describe the kinds of special objects contained in this Parcelable instance's marshaled
+     * representation.
+     * @return a bitmask indicating the set of special object types marshaled by this
+     * Parcelable object instance
+     */
     @Override
     public int describeContents() {
         return 0;
     }
 
+    /**
+     * Flatten this object in to a Parcel.
+     * @param dest the Parcel in which the object should be written
+     * @param flags additional flags about how the object should be written
+     */
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeFloat(altitude);
--- app/src/main/java/com/example/qlem/racemonitoring/graph/Speed.java
+++ app/src/main/java/com/example/qlem/racemonitoring/graph/Speed.java
@@ -21,29 +21,58 @@ public class Speed implements Parcelable {
         this.speed = speed;
     }
 
-    // TODO add headers
-
+    /**
+     * This constructor is used for retrieve the values that originally wrote into the Parcel.
+     * @param in the Parcel
+     */
     private Speed(Parcel in) {
         speed = in.readFloat();
     }
 
+    /**
+     * Interface that must be implemented and provided as a public CREATOR field that generates
+     * instances of the Parcelable class from a Parcel.
+     */
     public static final Creator<Speed> CREATOR = new Creator<Speed>() {
+
+        /**
+         * Create a new instance of the Parcelable class, instantiating it from the given Parcel
+         * whose data had previously been written by Parcelable.writeToParcel().
+         * @param in the Parcel to read the object's data from
+         * @return a new instance of the Parcelable class
+         */
         @Override
         public Speed createFromParcel(Parcel in) {
             return new Speed(in);
         }
 
+        /**
+         * Create a new array of the Parcelable class.
+         * @param size size of the array
+         * @return an array of the Parcelable class, with every entry initialized to null
+         */
         @Override
         public Speed[] newArray(int size) {
             return new Speed[size];
         }
     };
 
+    /**
+     * Describe the kinds of special objects contained in this Parcelable instance's marshaled
+     * representation.
+     * @return a bitmask indicating the set of special object types marshaled by this
+     * Parcelable object instance
+     */
     @Override
     public int describeContents() {
         return 0;
     }
 
+    /**
+     * Flatten this object in to a Parcel.
+     * @param dest the Parcel in which the object should be written
+     * @param flags additional flags about how the object should be written
+     */
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeFloat(speed);
